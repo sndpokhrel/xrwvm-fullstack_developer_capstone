c7183e19c1a663e47b38f127e959815e0ff3fa42
sndpokhrel <sandypokhrel09@gmail.com>
2024-08-03 07:22:54 -0400

    Static Page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,15 +1,20 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
 
     # path for dealer reviews view
 
